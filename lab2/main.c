31320c886c32f6b5e428822e8edf1cc3b0bbdf3a
Myralllka <Olli25010@gmail.com>
2019-10-08 00:14:56 +0300

    start lab2'

---
+++ lab2/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+#include "libb/library.c"
+
+int main(int argc, char *argv[]) {
+    int k = 0;
+    int n = 0;
+    my_str_t x;
+    my_str_create(&x, 0);
+
+//    for (int i = 0; i < 10; i++){
+//        scanf("%c", w+i);
+//    }
+
+    printf("k %d, n %d", k, n);
+    return 0;
+
+}
+
