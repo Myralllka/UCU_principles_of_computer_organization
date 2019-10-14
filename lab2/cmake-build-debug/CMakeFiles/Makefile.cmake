d73da34abe990045978873aa8993f8e1026ea2ec
Myralllka <Olli25010@gmail.com>
2019-10-14 23:33:06 +0300

    14.10.2019

--- lab2/cmake-build-debug/CMakeFiles/Makefile.cmake
+++ lab2/cmake-build-debug/CMakeFiles/Makefile.cmake
@@ -44,5 +44,6 @@ set(CMAKE_MAKEFILE_PRODUCTS
 
 # Dependency information for all targets:
 set(CMAKE_DEPEND_INFO_FILES
+  "CMakeFiles/libb.dir/DependInfo.cmake"
   "CMakeFiles/lab2.dir/DependInfo.cmake"
   )
--- lab2/cmake-build-debug/CMakeFiles/lab2.dir/DependInfo.cmake
+++ lab2/cmake-build-debug/CMakeFiles/lab2.dir/DependInfo.cmake
@@ -14,6 +14,7 @@ set(CMAKE_C_TARGET_INCLUDE_PATH
 
 # Targets to which this target links.
 set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/myralllka/Documents/PoCO/lab2/cmake-build-debug/CMakeFiles/libb.dir/DependInfo.cmake"
   )
 
 # Fortran module output directory.
