65d40abbbe2cd2970ef76a81bfbd048db7cdae51
Mykola <Olli25010@gmail.com>
2019-12-01 13:05:16 +0200

    a

---
+++ lab1stm/forstm/cmake-build-debug/CMakeFiles/forstm.elf.dir/cmake_clean.cmake
@@ -0,0 +1,30 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/forstm.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal.c.obj"
+  "CMakeFiles/forstm.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_cortex.c.obj"
+  "CMakeFiles/forstm.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_dma.c.obj"
+  "CMakeFiles/forstm.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_flash.c.obj"
+  "CMakeFiles/forstm.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_flash_ex.c.obj"
+  "CMakeFiles/forstm.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_gpio.c.obj"
+  "CMakeFiles/forstm.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_i2c.c.obj"
+  "CMakeFiles/forstm.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_i2c_ex.c.obj"
+  "CMakeFiles/forstm.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_pwr.c.obj"
+  "CMakeFiles/forstm.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_pwr_ex.c.obj"
+  "CMakeFiles/forstm.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_rcc.c.obj"
+  "CMakeFiles/forstm.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_rcc_ex.c.obj"
+  "CMakeFiles/forstm.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_tim.c.obj"
+  "CMakeFiles/forstm.elf.dir/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_tim_ex.c.obj"
+  "CMakeFiles/forstm.elf.dir/Src/gpio.c.obj"
+  "CMakeFiles/forstm.elf.dir/Src/main.c.obj"
+  "CMakeFiles/forstm.elf.dir/Src/stm32f3xx_hal_msp.c.obj"
+  "CMakeFiles/forstm.elf.dir/Src/stm32f3xx_it.c.obj"
+  "CMakeFiles/forstm.elf.dir/Src/syscalls.c.obj"
+  "CMakeFiles/forstm.elf.dir/Src/system_stm32f3xx.c.obj"
+  "CMakeFiles/forstm.elf.dir/startup/startup_stm32f303xc.s.obj"
+  "forstm.elf"
+  "forstm.elf.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang ASM C)
+  include(CMakeFiles/forstm.elf.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
