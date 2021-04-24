bbb2ca99b8b75e5c26b3b5112a549d937a30c87f
Mykola Morgunenko <Olli25010@gmail.com>
2021-04-24 23:58:35 +0200

    clear labs

--- lab2/main.h
+++
@@ -1,8 +0,0 @@
-#ifndef MAIN_H
-#define MAIN_H
-
-char to_lower(char c);
-int is_alpha(char c);
-void insertionSort(char *elements, size_t a, size_t b);
-void my_str_sort_words(my_str_t *str);
-#endif // MAIN_H
--- lab2/tests/c_check/c_check_reaload.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-checkmk test.check > test.c
